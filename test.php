232ec7ff61b752d109aaddcfbec9abdd1bf209ba
buddyff <rodrigocamparo@gmail.com>
2015-07-28 22:04:19 -0300

    Test de estilos javascripts

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+
+include ("head.php");
+
+
+?>
+
+<html>
+    <body>
+        <div class="fluid-container">
+            <div class="page-header">
+              <h1>Prueba para comprobar que carguen los .js y .css</h1>
+            </div>
+        </div>
+        
+        
+    </body>
+    
+    
+    
+    
+</html>
