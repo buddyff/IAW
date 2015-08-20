117df8ab717086c1b0916c07e2fb9574cc8b8cfe
buddyff <rodrigocamparo@gmail.com>
2015-08-20 19:48:40 -0300

    Index (solo visual)

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+include ("head.php");
+?>
+    
+<html>
+    <body class="body-index">
+          <div class="container-fluid">    
+             
+            <div class="col-lg-4 col-lg-offset-4" style="padding-top:250px;">
+                <div class="row">
+                    <div class="panel panel-index ">
+                        <div class="panel-heading text-center">
+                            Login
+                        </div>
+                         <div class="panel-body" >
+                            <form action="" method="">
+                                <div class="form-group">
+                                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                    <input class="form-control text-center" type="email" placeholder="Ingresá tu email" />
+                                </div>
+                                <div class="form-group">
+                                    <span class="input-group-addon"><i class="fa fa-key"></i></span>    
+                                    <input class="form-control text-center" type="password" placeholder="Ingresa tu password"/>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <span class="input-group-btn ">
+                                        <button type"submit" class="btn btn-default col-lg-6 pull-left">Ingresar   </button>
+                                        <button type"submit" class="btn btn-default col-lg-6 pull-right">Registrarse   </button>
+                                    </span>    
+                                </div>
+                               
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+        
+        
+    </body>
+    
+    
+    
+    
+</html>
